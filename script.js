f36da73c256dacdf42fa5587e6170fc7bff7a088
Spencer Laboda <spencerlaboda94@gmail.com>
2022-05-28 13:15:35 -0500

    Overhaul and simplification of operations begin

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const operatorButtons = document.querySelectorAll('.operator-button')
 const clearButton = document.querySelector('.clear')
 const equalButton = document.querySelector('.equals')
 const decimalButton = document.querySelector('.dec')
+const equalPushed = false
 
 
 
@@ -29,7 +30,7 @@ const opperations = {
 
 
 operatorButtons.forEach(op => op.addEventListener('click', function(e){
-    if(brain.operand == ''){
+    if(brain.operand == '' && !equalPushed){
     logOperations(e)
     brain.prevVal = brain.currentVal
     display.textContent = brain.prevVal
@@ -55,13 +56,21 @@ numberButtons.forEach(num => num.addEventListener('click', function(){
         display.textContent = ''
         operratorButtonPushed = !operratorButtonPushed
     }
+    if(display.textContent.length >= 9) return
     display.textContent += this.dataset.number
     brain.currentVal = display.textContent
     console.log(brain.currentVal)
 }))
 
-decimalButton.addEventListener('click', function(){
-    display.textContent += '.'
+decimalButton.addEventListener('click', function(e){
+    if(operratorButtonPushed){
+        display.textContent = ''
+        operratorButtonPushed = !operratorButtonPushed
+    }
+    if(display.textContent.length >= 9) return
+    display.textContent += "."
+    brain.currentVal = display.textContent
+    console.log(brain.currentVal)
 })
 
 
@@ -80,6 +89,7 @@ function clearLogic(){
     brain.logicHistory = []
     brain.prevVal = ''
     brain.currentVal = ''
+    brain.operand = ''
     console.log("ClearButton pushed", brain)
 }
 
@@ -93,16 +103,19 @@ function logOperations(e){
 }
 
 function performOperation(a, b, op){
-    return opperations[op](Number(a),Number(b))
+    return Math.round((opperations[op](Number(a),Number(b)))*100)/100
 }
 
 function equalButtonPress(){
     if(brain.prevVal == '') return
     let result = performOperation(Number(brain.prevVal), Number(brain.currentVal), brain.operand)
+    console.log("Hello", answerLengthAdjust(result))
     brain.prevVal = result
     brain.currentVal = ''
-    display.textContent = brain.prevVal
+    brain.operand = ''
+    display.textContent = answerLengthAdjust(Number(brain.prevVal))
     console.log(brain)
+    equalPushed = true
 
 }
 
@@ -124,4 +137,35 @@ function mult(a, b){
 function dvd(a, b){
     return a / b;
 }
+
+function answerLengthAdjust(num){
+    let zeros = 0;
+    if(String(num).length > 9){
+        let newNum = num
+       while(newNum % 10 == 0){
+           newNum /= 10
+           zeros+=1
+           console.log(newNum, num)
+       }
+       if(String(newNum).length > 6){
+           let roundingamount = String(newNum).length - 6
+           zeros += roundingamount
+           newNum = Math.round(newNum / 10**roundingamount)
+       }
+       console.log("zeros", zeros, "newNum", newNum)
+       zeros += String(newNum).length - 1
+       newNum = newNum / (10**(String(newNum).length - 1))
+       console.log("finalzeros", zeros, "finalnewNum", newNum)
+       return `${newNum}e${zeros}`
+    }
+    return num
+}
+
+//Visual Funcitons
+
+
+
+
+
+
 
